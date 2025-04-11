ffd282b2f0de7736cf19c64a485661518385a155
Vlad K. <vladyslav.kaminchuk@gmail.com>
2025-04-11 09:24:07 +0200

    Added theme colors

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-xl text-blue-500" font-bold>
+      <Text className="text-3xl text-light-100  " font-bold>
         Hello and Welcome!
       </Text>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+          300: "#374151",
+        },
+        accent: "#10B981",
+        neutral: "#374151",
+        "base-100": "#FFFFFF",
+        info: "#3ABFF8",
+        success: "#36D399",
+        warning: "#FBBD23",
+        error: "#F87272",
+      },
+    },
   },
   plugins: [],
 };
