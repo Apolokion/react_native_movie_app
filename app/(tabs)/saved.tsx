f0ef45bf3b21b49a029986fbbe6f798515ea5012
Vlad K. <vladyslav.kaminchuk@gmail.com>
2025-04-28 20:57:35 +0200

    Minor code refactor in saved.tsx

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, FlatList } from "react-native";
 import React from "react";
 import { images } from "@/constants/images";
 
