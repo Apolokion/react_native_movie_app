7e1513bd3ae8044c0fa2efb12256d4f8b6994f4a
Vlad K. <vladyslav.kaminchuk@gmail.com>
2025-04-28 18:36:39 +0200

    Added custom background to all screens

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { images } from "@/constants/images";
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="flex-1 absolute w-full z-0" />
     </View>
   );
 };
